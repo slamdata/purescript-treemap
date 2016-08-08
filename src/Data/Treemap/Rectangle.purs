3241b66d5d6bcb3eabd052ae2c4b64e8d360fa33
Gary Burgess <gary.burgess@gmail.com>
2016-08-08 12:13:56 +0100

    Add common instances for Rectangle and Cell

--- src/Data/Treemap/Rectangle.purs
+++ src/Data/Treemap/Rectangle.purs
@@ -5,12 +5,31 @@ import Prelude
 import Control.Comonad (class Comonad)
 import Control.Extend (class Extend)
 
+import Data.Generic (class Generic)
+
 -- | A rectangular region defined by: x position, y position, width, height.
 data Rectangle = Rectangle Number Number Number Number
+
+derive instance eqRectangle :: Eq Rectangle
+derive instance ordRectangle :: Ord Rectangle
+derive instance genericRectangle :: Generic Rectangle
+
+instance showRectangle :: Show Rectangle where
+  show (Rectangle x y w h) =
+    "(Rectangle "
+      <> show x <> " " <> show y <> " "
+      <> show w <> " " <> show h <> ")"
 
 -- | A rectangle associated with some data, used in the results of a treemap
 -- | computation.
 data Cell a = Cell Rectangle a
+
+derive instance eqCell :: Eq a => Eq (Cell a)
+derive instance ordCell :: Ord a => Ord (Cell a)
+derive instance genericCell :: Generic a => Generic (Cell a)
+
+instance showCell :: Show a => Show (Cell a) where
+  show (Cell rect a) = "(Cell " <> show rect <> " " <> show a <> ")"
 
 instance functorCell ∷ Functor Cell where
   map f (Cell rect a) = Cell rect (f a)
